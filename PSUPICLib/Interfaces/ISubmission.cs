77c6988b51695962dbedad271bac51ca1309b507
NontaponR <nontapon.r@phuket.psu.ac.th>
2018-01-03 15:40:54 +0700

    PSUPIC Library

---
+++ PSUPICLib/Interfaces/ISubmission.cs
@@ -0,0 +1,14 @@
+﻿using PSUPICLib.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PSUPICLib.Interfaces
+{
+    public interface ISubmission
+    {
+        Submission GetSubmissions(string conferenceID);
+    }
+}
