1a1cae92879b0bee3bcf654720401477c7be286f
NontaponR <nontapon.r@phuket.psu.ac.th>
2018-08-07 14:58:12 +0700

    แก้ไข Service URL ใหม่

--- Conference2018/Datasources/PSUPKTTimeAttendance.cs
+++ Conference2018/Datasources/PSUPKTTimeAttendance.cs
@@ -16,7 +16,7 @@ namespace Conference2018.Datasources
     /// </summary>
     public class PSUPKTTimeAttendance
     {
-        string _server = "https://tas-service.phuket.psu.ac.th"; //ที่ตั้งของ Service
+        string _server = "https://tas.phuket.psu.ac.th/api"; //ที่ตั้งของ Service
         string _applicationID = "p$up1c"; //รหัส Application ของ Partner: PSUPIC
         string _eventID = "cc11f90bc1e14291a228afa3762db417";
         int _schID = 1; //หมายเลข Schedule ในฐานของ Event นี้
